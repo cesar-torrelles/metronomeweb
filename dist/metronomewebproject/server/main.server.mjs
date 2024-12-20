b8c340f4dcfcaa2e39fa663fcd581a66e2335a64
Cesar Torrelles <raseksk8@gmail.com>
2024-12-20 12:44:08 +0100

    cambio configuracion para deploy

--- dist/metronomewebproject/server/main.server.mjs
+++ dist/metronomewebproject/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-CQRSRE3Y.mjs";import"./chunk-FF6XMPGJ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-6L7BA4C7.mjs";import"./chunk-WMDRQUYP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
